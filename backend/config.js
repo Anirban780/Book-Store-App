0045b1f09b987417fb08996464eb1f25842e8a14
Anirban Sarkar <137922245+Anirban780@users.noreply.github.com>
2024-09-21 23:42:52 +0530

    Initial Commit

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT= 5555;
+
+export const mongoDBURL= 
+    'mongodb+srv://Anirban_780:z3chh4vqNoPVGBcz@book-store-mern.1pvd5.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store-MERN';
